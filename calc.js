d880b29f2bad165bed6c7b5d834eae9f238ae671
ghanshyamjarvis <ghanshyam.jarvis@gmail.com>
2020-07-28 10:37:19 +0530

    multiplation add to new file

--- calc.js
+++ calc.js
@@ -5,3 +5,7 @@ exports.add = function (a, b) {
 exports.subtract = function (a, b) {
     return a-b;
 }; 
+
+exports.multiply = function (a, b) {
+    return a*b;
+}; 
---
+++ multi.js
@@ -0,0 +1,7 @@
+var calculator = require('./calc');
+
+var a=10, b=5;
+
+
+console.log("Multiplication : "+calculator.multiply(a,b));
+
