3a1bdb30ab5aa74d0b2f05abb33a6c45cef4d22b
ghanshyamjarvis <ghanshyam.jarvis@gmail.com>
2020-07-28 11:16:42 +0530

    name funcion

--- calc.js
+++ calc.js
@@ -9,3 +9,14 @@ exports.subtract = function (a, b) {
 exports.multiply = function (a, b) {
     return a*b;
 }; 
+
+
+
+var pet = 'cat';
+function myMethod() {
+     var pet = 'dog';
+     console.log(pet);
+ }
+
+myMethod();
+console.log(pet);
